4040de6c7d12dc094d66dcacecb06431a56f4671
pradeephrish <chaudharipradeep9@gmail.com>
2015-03-23 16:01:53 -0700

    created plane with object

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public float speed=100.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveHorizantal = Input.GetAxis ("Horizontal"); //x
+		float moveVertical = Input.GetAxis ("Vertical");   //y
+		
+		//Vector3 movement = new Vector3 (moveHorizantal,0.0f,0.0f);
+		//Rigidbody.AddForce (movement);
+		//GetComponent<Rigidbody>().AddForce (movement);
+		//transform.Translate(0, moveHorizantal * speed , 0);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveHorizantal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3  (moveHorizantal,0.0f,moveVertical);
+		//Rigidbody.AddForce (movement);
+		GetComponent<Rigidbody>().AddForce (movement*10);
+	}
+}
